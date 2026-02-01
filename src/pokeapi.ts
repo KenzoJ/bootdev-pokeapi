2debafb4e12f36ec0bf6a4d2d64df4730f88d185
KenzoJ <charles.felux@gmail.com>
2026-02-01 15:56:08 -0500

    refact: pokeapi class

--- src/pokeapi.ts
+++ src/pokeapi.ts
@@ -1,28 +1,21 @@
-//exports a Pokeapi Class
-// and types for return area JSON objects
-
 export class PokeAPI {
   private static readonly baseURL = "https://pokeapi.co/api/v2";
 
   constructor() {}
 
   async fetchLocations(pageURL?: string): Promise<ShallowLocations> {
-    const url = `${PokeAPI.baseURL}/location-area`;
-    try { 
-      const r = await fetch(url);
-      if (!r.ok) {
-        throw new Error(`Response: ${r.status}`);
+      try { 
+        const url = pageURL ?? `${PokeAPI.baseURL}/location-area`;
+        const r = await fetch(url);
+        if (!r.ok) {
+          throw new Error(`Response: ${r.status}`);
+        }
+        const result = await r.json();
+        return result;
+      } catch (error: any) {
+        console.error(error.message)
+        throw error;
       }
-      if (pageURL === "testing for failure") {
-        throw new Error(`Failure works`)
-      }
-      const result = await r.json();
-      return result;
-    } catch (error: any) {
-      console.error(error.message)
-      throw error;
-    }
-
    }
 
   //async fetchLocation(locationName: string): Promise<Location> {
@@ -31,23 +24,14 @@ export class PokeAPI {
 }
 
 export type ShallowLocations = {
-  count: number,
-  next: string,
-  previous: any,
-  results: Areas[]
+  count: number
+  next: string | null;
+  previous: string | null;
+  results: Location[]
 };
 
 export type Location = {
-  id: number
-  name: string
-  game_index: number
-  encounter_method_rates: string[]
-  location: Location
-  names: object[]
-  pokemon_encounters: object[] 
-};
-
-export type Areas = {
   name: string;
   url: string;
-}
+};
+
