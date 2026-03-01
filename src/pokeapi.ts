b7041d54e58890b82f0b862661256207631e201a
Kenzo Johnson <charles.felux@gmail.com>
2026-03-01 13:02:09 -0500

    temp: current work

--- src/pokeapi.ts
+++ src/pokeapi.ts
@@ -1,33 +1,44 @@
+import { Cache } from "./pokecache.ts";
+import type { cacheData } from "./pokecache.ts";
+
 export class PokeAPI {
   private static readonly baseURL = "https://pokeapi.co/api/v2";
 
-  constructor() {}
+  constructor(cacheInterval: number) {
+    this.cache = new Cache(cacheInterval);
+  }
 
   async fetchLocations(pageURL?: string): Promise<ShallowLocations> {
-      try { 
-        const url = pageURL ?? `${PokeAPI.baseURL}/location-area`;
-        const r = await fetch(url);
-        if (!r.ok) {
-          throw new Error(`Response: ${r.status}`);
-        }
-        const result = await r.json();
-        return result;
-      } catch (error: any) {
-        console.error(error.message)
-        throw error;
+    try {
+      let cacheData: cacheData | undefined = this.cache.get(pageURL);
+      if (cacheData != undefined) {
+        return cacheData;
       }
-   }
+      const url = pageURL ?? `${PokeAPI.baseURL}/location-area`;
+      const r = await fetch(url);
+      if (!r.ok) {
+        throw new Error(`Response: ${r.status}`);
+      }
+      const result = await r.json();
+      Cache.add(pageURL, result);
+      return result;
+    } catch (error: any) {
+      console.error(error.message);
+      throw error;
+    }
+  }{
+  { import = "lazyvim.plugins.extras.linting.eslint" },
+  { import = "lazyvim.plugins.extras.formatting.prettier" },
 }
 
 export type ShallowLocations = {
-  count: number
+  count: number;
   next: string | undefined;
   previous: string | undefined;
-  results: Location[]
+  results: Location[];
 };
 
 export type Location = {
   name: string;
   url: string;
 };
-
--- src/pokecache.ts
+++ src/pokecache.ts
@@ -1,7 +1,6 @@
 export type CacheEntry<T> = {
   createdAt: number;
-  val: <T>;
-
+  val: <T>
 }
 
 export class Cache {
@@ -11,36 +10,35 @@ export class Cache {
 
   constructor(tempNumber: number) {
     this.#interval = tempNumber;
-    this.#startReapLoop()
+    this.#startReapLoop
   }
 
-  add<T>(key: string, val: T): void{
-    let tempEntry:CacheEntry<T> = { createdAt: Date.now(), val}
+  add(key: string, val: <T>): void {
+    let tempEntry: CacheEntry<T> = { createdAt: Date.now(), val }
     this.#cache.set(key, tempEntry)
   }
 
-  get<T>(key: string):<any> {
+  get(key: string): <any> {
     let tempValue: <any> = this.#cache.get(key)
-    if (tempValue != undefined ) {
-      return tempValue.val; 
-  }
-    return undefined;
-  }
-
-  #reap(): void {
-    for (const [key, entry] of this.#cache) {
-      let timePassed: number = Date.now() - value.createdAt
-      if (timePassed > this.#interval) {
-        this.#cache.delete(key);
-      }
-    }
+if (tempValue != undefined) {
+  return tempValue.val;
+}
+return undefined;
   }
 
-  #startReapLoop(): void {
-    let intervalID: NodeJS.Timeout | undefined = setInterval(() => {
-      this.#reap() 
-    },this.#interval);
-    this.#reapIntervalId = intervalID;
+#reap(): void { for(const [key, entry] of this.#cache) {
+  let timePassed: number = Date.now() - value.createdAt
+  if (timePassed > this.#interval) {
+    this.#cache.delete(key);
+  };
+}
+  }
+
+#startReapLoop(): void {;
+  let intervalID: NodeJS.Timeout | undefined = setInterval(() => {
+  this.#reap()
+}, this.#interval);
+this.#reapIntervalId = intervalID;
   }
 
 }
