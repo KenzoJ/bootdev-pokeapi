b13f2118d811a99acbf4af4d7c2a4b1560d4ef68
Kenzo Johnson <charles.felux@gmail.com>
2026-03-04 07:52:42 -0500

    refact: pokeapi for assignment

--- src/pokeapi.ts
+++ src/pokeapi.ts
@@ -9,13 +9,19 @@ export class PokeAPI {
     this.cache = new Cache(cacheInterval);
   }
 
+  closeCache() {
+    this.cache.stopReapLoop();
+  }
+
   async fetchLocations(pageURL?: string): Promise<ShallowLocations> {
+    const url = pageURL ?? `${PokeAPI.baseURL}/location-area`;
+    const cached = this.cache.get<ShallowLocations>(url);
+
+    if (cached) {
+      return cached;
+    }
+
     try {
-      const url = pageURL ?? `${PokeAPI.baseURL}/location-area`;
-      let cacheData: ShallowLocations | undefined = this.cache.get(url);
-      if (cacheData != undefined) {
-        return cacheData;
-      }
       const r = await fetch(url);
       if (!r.ok) {
         throw new Error(`Response: ${r.status}`);
@@ -29,21 +35,88 @@ export class PokeAPI {
       throw error;
     }
   }
-  async fetchLocation(locationName: string): Promise<any> {
-    //TODO: Add a fetchLocation method, takes location name and returns <Promise<location>
-    //
+  async fetchLocation(locationName: string): Promise<Location> {
+    const url = `${PokeAPI.baseURL}/location-area/${locationName}`;
+    const cached = this.cache.get<Location>(url);
+    if (cached) {
+      return cached;
+    }
+    try {
+      const resp = await fetch(url);
+      if (!resp.ok) {
+        throw new Error(`${resp.status} ${resp.statusText}`);
+      }
+      const location: Location = await resp.json();
+      this.cache.add(url, location);
+      return location;
+    } catch (e) {
+      throw new Error(
+        `Error fetching location '${locationName}': ${(e as Error).message}`,
+      );
+    }
   }
 }
 //
-
 export type ShallowLocations = {
   count: number;
-  next: string | undefined;
-  previous: string | undefined;
-  results: Location[];
+  next: string;
+  previous: string;
+  results: {
+    name: string;
+    url: string;
+  }[];
 };
 
 export type Location = {
+  encounter_method_rates: {
+    encounter_method: {
+      name: string;
+      url: string;
+    };
+    version_details: {
+      rate: number;
+      version: {
+        name: string;
+        url: string;
+      };
+    }[];
+  }[];
+  game_index: number;
+  id: number;
+  location: {
+    name: string;
+    url: string;
+  };
   name: string;
-  url: string;
+  names: {
+    language: {
+      name: string;
+      url: string;
+    };
+    name: string;
+  }[];
+  pokemon_encounters: {
+    pokemon: {
+      name: string;
+      url: string;
+    };
+    version_details: {
+      encounter_details: {
+        chance: number;
+        condition_values: any[];
+        max_level: number;
+        method: {
+          name: string;
+          url: string;
+        };
+        min_level: number;
+      }[];
+      max_chance: number;
+      version: {
+        name: string;
+        url: string;
+      };
+    }[];
+  }[];
 };
+
